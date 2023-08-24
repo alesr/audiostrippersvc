0f54727a366fbbe9a275cc14456408bf41600314
Alessandro Resta <alessandro.resta@proton.me>
2023-08-24 19:29:27 +0300

    Use SSL cert for gRPC server

--- cmd/audiostripper/main.go
+++ cmd/audiostripper/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"log/slog"
 	"net"
 	"os"
@@ -10,32 +12,53 @@ import (
 	apiv1 "github.com/alesr/audiostripper/api/proto/audiostripper/v1"
 	"github.com/alesr/audiostripper/internal/app/audiostripper"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
-const grpcPort string = ":50051"
+const (
+	grpcPort string = ":50051"
+	certPath string = "/etc/ssl/mycerts/cert.pem"
+	keyPath  string = "/etc/ssl/mycerts/key.pem"
+)
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 	}))
 
-	grpcServer := grpc.NewServer()
+	var useSSL bool
+	flag.BoolVar(&useSSL, "ssl", false, "Use SSL for the gRPC server")
+	flag.Parse()
+
+	var serverOpts []grpc.ServerOption
+
+	if useSSL {
+		creds, err := credentials.NewServerTLSFromFile(certPath, keyPath)
+		if err != nil {
+			log.Fatalf("Failed to load SSL certificates: %v", err)
+		}
+		serverOpts = append(serverOpts, grpc.Creds(creds))
+	}
+
+	grpcServer := grpc.NewServer(serverOpts...)
 
 	grpcServer.RegisterService(
 		&apiv1.AudioStripper_ServiceDesc,
 		api.NewGRPCServer(logger, audiostripper.NewService(logger)),
 	)
 
+	logger.Info("Starting gRPC server", "grpc_port", grpcPort)
+
 	lis, err := net.Listen("tcp", grpcPort)
 	if err != nil {
 		logger.Error("Could not listen", "grpc_port", grpcPort, "error", err)
-		os.Exit(1)
+		os.Exit(2)
 	}
 
 	go func() {
 		if err := grpcServer.Serve(lis); err != nil {
 			logger.Error("Failed to serve gRPC server", "error", err)
-			os.Exit(2)
+			os.Exit(3)
 		}
 	}()
 
