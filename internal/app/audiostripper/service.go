42503384ef84e11b0ca6112fe2298c641569dc3a
Alessandro Resta <alessandro.resta@proton.me>
2023-08-26 13:52:29 +0300

    Implement audiostripper service

--- internal/app/audiostripper/service.go
+++ internal/app/audiostripper/service.go
@@ -1,33 +1,71 @@
 package audiostripper
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"log/slog"
 )
 
-type Service struct {
-	logger *slog.Logger
-}
-
-func NewService(logger *slog.Logger) *Service {
-	return &Service{
-		logger: logger,
-	}
-}
-
 type (
+	// ExtractAudioInput defines the input for the ExtractAudio method.
 	ExtractAudioInput struct {
-		Data []byte
+		SampleRate string
+		FilePath   string
 	}
 
+	// ExtractAudioOutput defines the output for the ExtractAudio method
 	ExtractAudioOutput struct {
-		Data []byte
+		FilePath string
+	}
+
+	ExtractCmdParams struct {
+		InputFile, OutputFile, SampleRate string
+		Stderr                            *bytes.Buffer
+	}
+
+	// ExtractCmd is a function that runs the extractor command.
+	ExtractCmd func(params *ExtractCmdParams) error
+
+	// Service provides methods for extracting audio from a video file.
+	Service struct {
+		logger *slog.Logger
+		cmd    ExtractCmd
 	}
 )
 
+// New creates a new Service.
+func New(logger *slog.Logger, cmd ExtractCmd) *Service {
+	return &Service{
+		logger: logger,
+		cmd:    cmd,
+	}
+}
+
+// ExtractAudio extracts audio from a video file.
 func (s *Service) ExtractAudio(ctx context.Context, in *ExtractAudioInput) (*ExtractAudioOutput, error) {
-	s.logger.Info("Extracting audio", "input", in)
+	cmdParams := ExtractCmdParams{
+		InputFile:  in.FilePath,
+		OutputFile: outputFilePath(in.FilePath),
+		SampleRate: in.SampleRate,
+		Stderr:     &bytes.Buffer{},
+	}
+
+	s.logger.Debug("Running extractor command", "params", cmdParams)
+
+	if err := s.cmd(&cmdParams); err != nil {
+		s.logger.Error("Could not run extractor command",
+			slog.String("error", err.Error()),
+			slog.String("stderr", cmdParams.Stderr.String()),
+		)
+		return nil, fmt.Errorf("could not run extractor command: %s", err)
+	}
+
 	return &ExtractAudioOutput{
-		Data: in.Data,
+		FilePath: cmdParams.OutputFile,
 	}, nil
+}
+
+func outputFilePath(in string) string {
+	return in[:len(in)-4] + ".wav"
 }
